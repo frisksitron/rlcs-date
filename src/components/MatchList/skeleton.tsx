887583bef561d3d21bbb8420aea8ed7174aa2c6c
Nataniel Pedersen <frisksitron@users.noreply.github.com>
2022-08-13 22:15:35 +0200

    fix: make skeleton mobile friendly

--- src/components/MatchList/skeleton.tsx
+++ src/components/MatchList/skeleton.tsx
@@ -6,22 +6,27 @@ const MatchListSkeleton = () => {
       {[1, 2, 3, 4].map((match, index) => {
         const rowColor = index % 2 === 0 ? "bg-base-100" : "bg-base-200";
 
-        const widths = ["w-20", "w-24", "w-28", "w-32", "w-36"];
+        const widths = [
+          ["w-20", "w-6"],
+          ["w-24", "w-8"],
+          ["w-28", "w-10"],
+          ["w-32", "w-12"],
+          ["w-36", "w-16"],
+        ];
         const randomWidth = () =>
           widths[Math.floor(Math.random() * widths.length)];
-        const randomWidths = [
-          randomWidth(),
-          randomWidth(),
-          randomWidth(),
-          randomWidth(),
-        ];
+        const randomWidths = [randomWidth(), randomWidth()];
 
         return (
           <div
             key={index}
             className={`flex justify-between lg:px-10 p-4 first:rounded-t-lg last:rounded-b-lg ${rowColor}`}
           >
-            <div className={"w-20 w-24 w-28 w-32 w-36 hidden"}></div>
+            <div
+              className={
+                "w-6 w-8 w-10 w-12 w-16 w-20 w-24 w-28 w-32 w-36 hidden"
+              }
+            ></div>
             <div className="w-5/12">
               <div className="flex items-center">
                 <div className="avatar">
@@ -30,12 +35,12 @@ const MatchListSkeleton = () => {
                 <div className={"pl-2 md:pl-5"}>
                   <div className="lg:hidden overflow-hidden">
                     <div
-                      className={`${randomWidths[0]} h-6 bg-slate-500 animate-pulse rounded-sm`}
+                      className={`${randomWidths[0][1]} h-6 bg-slate-500 animate-pulse rounded-sm`}
                     ></div>
                   </div>
                   <div className="hidden lg:block">
                     <div
-                      className={`${randomWidths[1]} h-6 bg-slate-500 animate-pulse rounded-sm`}
+                      className={`${randomWidths[0][0]} h-6 bg-slate-500 animate-pulse rounded-sm`}
                     ></div>
                   </div>
                 </div>
@@ -51,12 +56,12 @@ const MatchListSkeleton = () => {
                 <div className={"pr-2 md:pr-5"}>
                   <div className="lg:hidden overflow-hidden">
                     <div
-                      className={`${randomWidths[2]} h-6 bg-slate-500 animate-pulse rounded-sm`}
+                      className={`${randomWidths[1][1]} h-6 bg-slate-500 animate-pulse rounded-sm`}
                     ></div>
                   </div>
                   <div className="hidden lg:block">
                     <div
-                      className={`${randomWidths[3]} h-6 bg-slate-500 animate-pulse rounded-sm`}
+                      className={`${randomWidths[1][0]} h-6 bg-slate-500 animate-pulse rounded-sm`}
                     ></div>
                   </div>
                 </div>
